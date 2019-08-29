9989fca0cb5ab501b8b0366b2a563d6e4810a16d
kamikeeen <unjo.kamiken@gmail.com>
2019-08-29 21:59:23 +0900

    create-Asynchronous communication

--- app/assets/javascripts/create.js
+++ app/assets/javascripts/create.js
@@ -1,4 +1,28 @@
 $(function(){
+
+  function appendMessage(message){
+    if (message.image){
+      var image = `<img src=${message.image}></img>`
+    }else{
+      var image = ""
+    }
+
+    var html = `<div class="chat__main__name">
+                  ${message.user_name}
+                </div>
+                <div class="chat__main__day">
+                  ${message.date}
+                </div>
+                <div class="chat__main__text">
+                  ${message.body}
+                </div>
+                <div class="chat__main__image">
+                  ${image}
+                </div>`
+                
+    return html;
+  }
+
   $("#new_message").on("submit", function(e){
     e.preventDefault();
     var url = $("#new_message").attr(".action");
@@ -12,5 +36,17 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(message){
+      var html = appendMessage(message);
+      $(".chat__main").append(html);
+      $("#message_body").val("");
+      $("#message_image").val("");
+      $("input[type='submit']").attr("disabled", false);
+      $(".chat__main").animate({scrollTop: $(".chat__main")[0].scrollHeight}, 'slow');
+    })
+    .fail(function(){
+      alert("oh my god");
+      $("input[type='submit']").attr("disabled", false);
+    })
   })
-})
+});
