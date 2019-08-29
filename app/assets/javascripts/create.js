95c2452d8f4bc5e7fbb9e37d7c6c3490e8289de8
kamikeeen <unjo.kamiken@gmail.com>
2019-08-29 16:47:41 +0900

    send-message_use-ajax

---
+++ app/assets/javascripts/create.js
@@ -0,0 +1,16 @@
+$(function(){
+  $("#new_message").on("submit", function(e){
+    e.preventDefault();
+    var url = $("#new_message").attr(".action");
+    var formData = new FormData(this);
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
+  })
+})
