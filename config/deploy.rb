4555f9b7ab305cbb718529f60c4f4f24aaf0c82d
kamikeeen <unjo.kamiken@gmail.com>
2019-09-03 15:07:36 +0900

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.1"
 
 set :application, "chat-space"
-set :repo_url, "git@github.com/kamikeeen/chat-space.git"
+set :repo_url, "https://github.com/kamikeeen/chat-space"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
